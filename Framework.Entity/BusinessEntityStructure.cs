67cd1c53057025c43beb4bbc7768bada00d6c7e9
Jose Carlos de Jesus Junior <juninho_web@hotmail.com>
2018-07-05 14:40:22 -0300

    Add files via upload

---
+++ Framework.Entity/BusinessEntityStructure.cs
@@ -0,0 +1,173 @@
+using System;
+using System.Collections.Generic;
+using Framework.Core;
+
+namespace Framework.Entity
+{
+    /// <summary>
+    /// It provides a base class called Business Entities Structure (Base Model) used to map automatticaly the properties without using reflection from the database/datasource
+    /// </summary>
+    /// <example>
+    ///     <code>
+    ///     [Serializable]
+    ///     public class UsuarioBES : BussinessEntityStructure
+    ///     {
+    ///         #region| Properties |
+    ///         
+    ///         public int ID { get; set; }
+    ///         public string Nome { get; set; }
+    ///
+    ///         #endregion
+    ///         
+    ///         public UsuarioBES()
+    ///         {
+    ///             Map("CD_ID", "ID");
+    ///             Map("Nome");
+    ///         }
+    ///     }
+    ///     </code>
+    /// </example>
+    [Serializable]
+    public abstract class BusinessEntityStructure : IDisposable
+    {
+        #region| Fields |
+
+        [NonSerialized]
+        private List<MapperProperty> oProperties = null;
+
+        #endregion
+
+        #region| Properties | 
+
+        /// <summary>
+        /// Stores the class properties mapping relationship
+        /// </summary>
+        [System.Xml.Serialization.XmlIgnore]
+        public List<MapperProperty> MappedProperties
+        {
+            get
+            {
+                return this.oProperties;
+            }
+            set
+            {
+                this.oProperties = value;
+            }
+        }
+
+        #endregion
+
+        #region| Constructor |
+
+        /// <summary>
+        /// Bussiness Entity Structure Construtor
+        /// </summary>
+        protected BusinessEntityStructure()
+        {
+
+        }
+
+        #endregion
+
+        #region| Destructor | 
+
+        /// <summary>
+        /// Default destructor
+        /// </summary>
+        ~BusinessEntityStructure()
+        {
+            this.Dispose();
+        }
+
+        #endregion
+
+        #region| Methods |
+
+        /// <summary>
+        /// Maps the class property to be filled with the datasource column
+        /// </summary>
+        /// <param name="ColumnName">Column Name</param>
+        /// <param name="PropertyName">Property Name</param>
+        /// <example>
+        /// <code>
+        ///     public UsuarioBES()
+        ///     {
+        ///         Map("CD_ID", "ID");
+        ///         Map("DS_NAME","Nome");
+        ///     }
+        /// </code>
+        /// </example>
+        protected void Map(string ColumnName, string PropertyName)
+        {
+            if (ColumnName.IsNotNull() && PropertyName.IsNotNull())
+            {
+                this.AddMapping(ColumnName, PropertyName);
+            }
+        }
+
+        /// <summary>
+        /// Maps the class property to be filled with the datasource column
+        /// </summary>
+        /// <remarks>
+        /// The property name must be the same to the datasource column name
+        /// </remarks>
+        /// <param name="PropertyName">Property Name</param>
+        /// <example>
+        /// <code>
+        ///     public UsuarioBES()
+        ///     {
+        ///         Map("ID");
+        ///         Map("Nome");
+        ///     }
+        /// </code>
+        /// </example>
+        protected void Map(string PropertyName)
+        {
+            if (PropertyName.IsNotNull())
+            {
+                this.AddMapping(PropertyName, PropertyName);
+            }
+        }
+
+        /// <summary>
+        /// Maps the class property to be filled with the datasource column
+        /// </summary>
+        /// <param name="ColumnName">Column Name</param>
+        /// <param name="PropertyName">Property Name</param>
+        private void AddMapping(string ColumnName, string PropertyName)
+        {
+            this.Check();
+
+            if (!this.MappedProperties.Exists(P => P.PropertyName == PropertyName))
+            {
+                this.MappedProperties.Add(new MapperProperty(ColumnName, PropertyName));
+            }
+        }
+
+        /// <summary>
+        /// Check if the properties collections is initialized
+        /// </summary>
+        private void Check()
+        {
+            if (this.MappedProperties.IsNull())
+            {
+                this.MappedProperties = new List<MapperProperty>();
+            }
+        }
+
+        #endregion
+
+        #region| IDisposable Members |
+
+        /// <summary>
+        /// Release allocated resource
+        /// </summary>
+        public void Dispose()
+        {
+            this.MappedProperties = null;
+            this.oProperties = null;
+        }
+
+        #endregion
+    }
+}
---
+++ Framework.Entity/Extensions/Extensions.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.Data;
+
+using Framework.Core;
+
+namespace Framework.Entity.Extensions
+{
+    /// <summary>
+    /// Providers Extension Methods that are a special kind of static method, 
+    /// but they are called as if they were instance methods on the extended type. 
+    /// </summary>
+    public static partial class Extensions
+    {
+        #region| Methods |
+
+        /// <summary>
+        /// Converts a generic list into a DataTable
+        /// </summary>
+        /// <typeparam name="T">Type of objects int list</typeparam>
+        /// <param name="List">List to be converted</param>
+        /// <returns>Converted DataTable</returns>
+        public static DataTable ToDataTable<T>(this List<T> @List) where T : BusinessEntityStructure
+        {
+            var oDataTable = new DataTable();
+
+            if (@List.IsNotNull())
+            {
+                var oEntity = @List[0];
+
+                if (oEntity.IsNotNull() && oEntity.MappedProperties.IsNotNull())
+                {
+                    for (int i = 0; i < oEntity.MappedProperties.Count; i++)
+                    {
+                        var item = oEntity.MappedProperties[i];
+
+                        oDataTable.Columns.Add(item.PropertyName);
+                    }
+
+                    if (oDataTable.Columns.Count > 0)
+                    {
+                        for (int i = 0; i < @List.Count; i++)
+                        {
+                            T item = @List[i];
+
+                            DataRow oRow = oDataTable.NewRow();
+
+                            for (int y = 0; i < item.MappedProperties.Count; y++)
+                            {
+                                var prop = item.MappedProperties[y];
+
+                                var oPropertyInfo = item.GetType().GetProperty(prop.PropertyName);
+
+                                if (oPropertyInfo.CanRead)
+                                {
+                                    oRow[prop.PropertyName] = oPropertyInfo.GetValue(item, null);
+                                }
+                            }
+
+                            oDataTable.Rows.Add(oRow);
+                        }
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+            }
+            else
+            {
+                return null;
+            }
+
+            return oDataTable;
+        } 
+        
+        #endregion
+    }
+}
